0dcd407966c1fc624f8b5afb45e4eff8bb605de7
Zack Jackson <z.jackson58@gmail.com>
2019-01-11 22:57:20 -0500

    Express server with nodemailer set up

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const sendMail = require('./mailer/mailer');
+
+const app = express();
+
+const publicPath = path.join(__dirname, '../client', 'public');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(publicPath));
+app.use('/', sendMail);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}`);
+});
---
+++ server/mailer/mailer.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const credentials = require('./mailerConfig');
+const router = express.Router();
+
+const transport = {
+  host: 'smtp.gmail.com',
+  auth: {
+    user: credentials.USER,
+    pass: credentials.PASSWORD
+  }
+};
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Server ready to send email');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const content = `name: ${name} \n email: ${email} \n message: ${message}`;
+  const mail = {
+    from: name,
+    to: 'z.jackson58@gmail.com',
+    subject: 'New message from Portfolio Site',
+    text: content
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        message: 'Send message failed.'
+      });
+    } else {
+      res.json({
+        message: 'Message sent successfully!'
+      });
+    }
+  });
+});
+
+module.exports = router;
