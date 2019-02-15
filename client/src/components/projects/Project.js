931abc044cd2eedd81d38acb4f4249954fa44b74
Zack Jackson <z.jackson58@gmail.com>
2019-02-14 23:19:09 -0500

    Projects gallery styles

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 const Project = props => {
   return (
@@ -9,11 +10,21 @@ const Project = props => {
         className="project__preview-image"
       />
       <div className="project__actions">
-        <button className="button project__button">Live Demo</button>
-        <button className="button project__button">View Code</button>
+        <button
+          className="button button--small project__button"
+          onClick={() => window.open(props.liveLink, '_blank')}
+        >
+          Live Demo
+        </button>
+        <button
+          className="button button--small project__button"
+          onClick={() => window.open(props.codeLink, '_blank')}
+        >
+          View Code
+        </button>
       </div>
     </div>
   );
 };
 
-export default Project;
+export default withRouter(Project);
--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
 import Project from './Project';
-import toneShop from '../../images/Tone-Shop_preview.jpg';
-import slackr from '../../images/Slackr_preview.jpg';
-import weather from '../../images/Weather_preview.jpg';
+import toneShop from '../../images/Tone-shop-preview.png';
+import harmoney from '../../images/Harmoney-preview.png';
+import slackr from '../../images/Slackr-preview.png';
+import weather from '../../images/Weather-preview.png';
+
+const projectDetails = [
+  {
+    name: toneShop,
+    image: toneShop,
+    liveLink: 'https://tone-shop.herokuapp.com/',
+    codeLink: 'https://github.com/zackjacksongit/Tone-Shop'
+  },
+  {
+    name: weather,
+    image: weather,
+    liveLink: 'http://colorful-ocean.surge.sh/',
+    codeLink: 'https://github.com/zackjacksongit/Weather'
+  },
+  {
+    name: harmoney,
+    image: harmoney,
+    liveLink: 'https://harmoney-finance.herokuapp.com/',
+    codeLink: 'https://github.com/zackjacksongit/HarmoneyV2'
+  },
+  {
+    name: slackr,
+    image: slackr,
+    liveLink: 'https://slackr-react.herokuapp.com/',
+    codeLink: 'https://github.com/zackjacksongit/Slackr'
+  }
+];
 
 const Projects = () => {
   return (
@@ -12,10 +40,14 @@ const Projects = () => {
       <div className="projects__rectangle heading__rectangle" />
 
       <div className="projects__gallery">
-        <Project image={toneShop} />
-        <Project image={slackr} />
-        <Project image="" />
-        <Project image={weather} />
+        {projectDetails.map(project => (
+          <Project
+            key={project.name}
+            image={project.image}
+            liveLink={project.liveLink}
+            codeLink={project.codeLink}
+          />
+        ))}
       </div>
     </div>
   );
